6699f1ccde9abb40a58667bf09d15ee4546ab05c
deem <michalkornel4@gmail.com>
2025-07-17 13:52:29 +0200

    missing app.use(express.json())

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const { logToDiscord } = require('./utils/discord');
 
 const app = express();
 
+app.use(express.json());
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '..', 'views'));
 app.use(express.static(path.join(__dirname, '..', 'public')));
